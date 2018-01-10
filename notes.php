3ffe790ad93607196d7234e8fdccdd3ab9972289
Alena Bukilić <bukilicalena@gmail.com>
2018-01-10 15:45:14 +0100

    Add files via upload

--- notes.php
+++ notes.php
@@ -20,34 +20,36 @@
           $db->close();
          }
     }
-    else if(isset($_GET['recordId'])) {
-        
-          $id = $db->real_escape_string($_GET['recordId']);
+    else if(isset($_POST['delete'])){
+          $postid = $_POST['delete'];
           
-          $sql = "DELETE FROM recipes WHERE postid=$id"; 
+          $sql = "DELETE FROM recipes WHERE postid=$postid AND id=$id"; 
          
           if ($db->query($sql) === true) {
               header( "location: notes.php" );
           }
           
           $db->close();
+        
     }
+    
     else if(isset($_POST['update'])){
         
         if(!empty($_POST['recepie'])){
-          $count = count($_POST['update']);
-          if($count>0){
-          for($i=0;$i<$count;$i++){
-              $recepie = $db->real_escape_string($_POST['recepie']);
-              $sql = "UPDATE recipes SET recepie='$recepie' WHERE postid='$_POST[update][$i]'";
-              if ($db->query($sql) === true) {
-                header( "location: notes.php" );
-              }
-                
-              $db->close();
+      
+            $postid = $_POST['update'];
+            
+            $recepie = $db->real_escape_string($_POST['recepie']);
+            
+            $sql = "UPDATE recipes SET recepie='$recepie' WHERE postid=$postid AND id=$id";
+            
+            if ($db->query($sql) === true) {
+              header( "location: notes.php" );
             }
-          }
+              
+            $db->close();
         }
+
     }
     else if(isset($_POST['postCategory'])){
       
@@ -57,7 +59,7 @@
           for($i=0;$i<$count;$i++){
               
               $category = $_POST['nameValue'];
-              $sql = "UPDATE recipes SET category='$category' WHERE postid='$_POST[postCategory][$i]'";
+              $sql = "UPDATE recipes SET category='$category' WHERE postid='$_POST[postCategory][$i]' AND id=$id";
               if ($db->query($sql) === true) {
                 header( "location: notes.php" );
               }
@@ -84,14 +86,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>ProCooking</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-	<link href="https://fonts.googleapis.com/css?family=Merienda+One|Sedgwick+Ave" rel="stylesheet">
+  <title>Final CS50</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Merienda+One|Sedgwick+Ave" rel="stylesheet">
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body id="notes">
 <nav id="navNotes" class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -130,42 +132,42 @@
         $result = $db->query("SELECT * FROM recipes WHERE category='$name' AND id='$id'");
           
         while($user=$result->fetch_assoc()){
-        	     echo "<div class='words'>
+               echo "<div class='words'>
             <div class='realtext' spellcheck='false' contenteditable>$user[recepie]</div>
             <span  name='$user[category]' class='smallCat firstCatBut'>$user[category]</span>
             <button type='submit' name='update' value='$user[postid]' class='savebutton'><span class='fa fa-floppy-o'></span></button>
-            <a href='notes.php?recordId=$user[postid]' class='deletebutton'><span class='fa fa fa-trash-o'></span></a>
-          </div>";
-	    }
+            <button type='submit' name='delete' value='$user[postid]' class='deletebutton'><span class='fa fa-trash-o'></span></button>
+            </div>";
+      }
     }
     else {
-	   while($user=$result->fetch_assoc()){
-	      if(!empty($user[category])){
-        	     echo "<div class='words'>
+     while($user=$result->fetch_assoc()){
+        if(!empty($user[category])){
+               echo "<div class='words'>
             <div class='realtext' spellcheck='false' contenteditable>$user[recepie]</div>
             <span  name='$user[category]' class='smallCat firstCatBut'>$user[category]</span>
             <button type='submit' name='update' value='$user[postid]' class='savebutton'><span class='fa fa-floppy-o'></span></button>
-            <a href='notes.php?recordId=$user[postid]' class='deletebutton'><span class='fa fa fa-trash-o'></span></a>
+            <button type='submit' name='delete' value='$user[postid]' class='deletebutton'><span class='fa fa-trash-o'></span></button>
           </div>";
-	      }
-	      else {
-	          echo "<div class='words'>
+        }
+        else {
+            echo "<div class='words'>
             <div class='realtext' spellcheck='false' contenteditable>$user[recepie]</div>
             <span  name='cat' class='smallCat firstCatBut'>Category</span>
             <button type='submit' name='update' value='$user[postid]' class='savebutton'><span class='fa fa-floppy-o'></span></button>
-            <a href='notes.php?recordId=$user[postid]' class='deletebutton'><span class='fa fa fa-trash-o'></span></a>
-          </div>";
-	      }
-	   }
+            <button type='submit' name='delete' value='$user[postid]' class='deletebutton'><span class='fa fa-trash-o'></span></button>
+           </div>";
+        }
      }
-	 ?>
-	 <textarea id="onlyText" type='text' style='display:none' name='recepie'></textarea>
-	 </div>
+     }
+   ?>
+   <textarea id="onlyText" type='text' style='display:none' name='recepie'></textarea>
+   </div>
 </form>
 
 <script type="text/javascript" src="script.js"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+<script src="//code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 </body>
 </html>
--- register.php
+++ register.php
@@ -10,7 +10,7 @@
             $email = $db->real_escape_string($_POST['email']); 
             $password = $db->escape_string(password_hash($_POST['password'], PASSWORD_BCRYPT));
             
-            $result = $db->query("SELECT * FROM users WHERE email='$email'") or die($db->error());
+            $result = $db->query("SELECT 1 FROM users WHERE email='$email'") or die($db->error());
             
             if($result->num_rows > 0){
               $_SESSION['message'] = 'User with this email already exists!';
@@ -24,7 +24,7 @@
                   $_SESSION[ 'message' ] = "Registration succesful!";
                   $_SESSION['name'] = $_POST['name']; 
                   
-                  $result = $db->query("SELECT * FROM users WHERE email='$email'");
+                  $result = $db->query("SELECT id FROM users WHERE email='$email'");
                   $user = $result->fetch_assoc();
                   $_SESSION['id'] = $user['id'];
                   
@@ -63,10 +63,10 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
-        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="about.php">About</a>
+        <a class="nav-link" href="about.html">About</a>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
@@ -92,8 +92,8 @@
   <button type="submit" name="register" class="btn btn-primary">Register</button>
 </form>
 
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+<script src="//code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 </body>
 </html>
